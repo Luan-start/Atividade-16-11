2cd099a6b989b29ac0cab61f2ff2c80b14d62d9f
LuanS <luanh.schier@gmail.com>
2023-11-24 08:02:44 -0300

    ...

--- Atividade-java-16-11-main/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Atividade-java-16-11-main/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-
 @SpringBootApplication
 public class DemoApplication {
 
